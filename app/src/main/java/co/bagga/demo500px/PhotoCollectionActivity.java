dd54cdbc873124ca058dcea12e3574b24732a456
Lalit Bagga <davin12x@gmail.com>
2017-07-26 19:32:33 -0400

    project init

---
+++ app/src/main/java/co/bagga/demo500px/PhotoCollectionActivity.java
@@ -0,0 +1,13 @@
+package co.bagga.demo500px;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoCollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_collection);
+    }
+}
