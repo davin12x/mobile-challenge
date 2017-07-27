b754e461aacfa8848144da73c21b9d3d096fb49e
Lalit Bagga <davin12x@gmail.com>
2017-07-26 20:13:53 -0400

    setup view holder to display data

---
+++ app/src/main/java/co/bagga/demo500px/Model/Photo.java
@@ -0,0 +1,10 @@
+package co.bagga.demo500px.Model;
+
+/**
+ * Created by bagga on 2017-07-26.
+ *
+ * Represent information of photo
+ */
+
+public class Photo {
+}
---
+++ app/src/main/java/co/bagga/demo500px/Viewholders/ImageCollectionViewHolder.java
@@ -0,0 +1,31 @@
+package co.bagga.demo500px.Viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import co.bagga.demo500px.Model.Photo;
+import co.bagga.demo500px.R;
+
+/**
+ * Created by bagga on 2017-07-26.
+ *
+ * View Holder used in Image Collection
+ */
+
+public class ImageCollectionViewHolder extends RecyclerView.ViewHolder {
+
+    private ImageView mProductImageView;
+    private TextView mProductNameTextView, mFollowersCountImageView ;
+
+    public ImageCollectionViewHolder(View itemView) {
+        super(itemView);
+
+        mProductImageView = itemView.findViewById(R.id.product_image);
+        mProductNameTextView = itemView.findViewById(R.id.product_name);
+        mFollowersCountImageView = itemView.findViewById(R.id.follower_count);
+    }
+
+    public void updateView(Photo photo) {}
+}
